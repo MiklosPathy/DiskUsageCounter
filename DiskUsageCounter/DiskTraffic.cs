d01c193571c9fa6d6dc095cbe6e54bcf8f06117f
Miklós Pathy <miklos.pathy@pulispace.com>
2018-03-13 06:01:16 +0100

    Initial again

---
+++ DiskUsageCounter/DiskTraffic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Timers;
+using System.ComponentModel;
+
+namespace DiskUsageCounter
+{
+    public class DiskTraffic : INotifyPropertyChanged
+    {
+        public Int64 Reads { get; set; }
+        public Int64 Writes { get; set; }
+
+        private PerformanceCounter diskReadCounter = new PerformanceCounter();
+        private PerformanceCounter diskWriteCounter = new PerformanceCounter();
+        private Timer mytimer = new Timer();
+
+        private void TimerElapsed(object source, ElapsedEventArgs e)
+        {
+            Reads = Reads + (Int64)diskReadCounter.NextValue();
+            Writes = Writes + (Int64)diskWriteCounter.NextValue();
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Reads"));
+                PropertyChanged(this, new PropertyChangedEventArgs("Writes"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DiskTraffic()
+        {
+            diskReadCounter.CategoryName = "PhysicalDisk";
+            diskReadCounter.CounterName = "Disk Read Bytes/Sec";
+            diskReadCounter.InstanceName = "_Total";
+            diskWriteCounter.CategoryName = "PhysicalDisk";
+            diskWriteCounter.CounterName = "Disk Write Bytes/Sec";
+            diskWriteCounter.InstanceName = "_Total";
+
+            mytimer.Elapsed += TimerElapsed;
+            mytimer.Interval = 1000;
+            mytimer.Enabled = true;
+        }
+    }
+}
---
+++ DiskUsageCounter/FormatKbSizeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DiskUsageCounter
+{
+    //Converter thx to
+    //https://thomasfreudenberg.com/archive/2017/01/21/presenting-byte-size-values-in-wpf/
+    public class FormatKbSizeConverter : IValueConverter
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        private static extern long StrFormatByteSizeW(long qdw, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszBuf,
+            int cchBuf);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var number = System.Convert.ToInt64(value);
+            var sb = new StringBuilder(32);
+            StrFormatByteSizeW(number, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
